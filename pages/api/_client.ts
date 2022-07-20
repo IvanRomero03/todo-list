04a9bb4b2756c1a6495643a1f57da7fd0e5e291d
IvanRomero03 <i.wells.ar@gmail.com>
2022-07-20 15:52:26 -0700

    global client

--- pages/api/_client.ts
+++ pages/api/_client.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const client = axios.create({
-  //baseURL: "https://to-do-app-back-end-express.herokuapp.com",
-  baseURL: "http://localhost:5000",
+  baseURL: "https://to-do-app-back-end-express.herokuapp.com",
+  //baseURL: "http://localhost:5000",
   headers: {
     "Content-Type": "application/json",
   },
