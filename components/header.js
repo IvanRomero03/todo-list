4d264b34a78d33a56eda93a5b40eb65a1e2aff22
IvanRomero03 <i.wells.ar@gmail.com>
2022-08-15 18:48:46 -0500

    header update

--- components/header.js
+++ components/header.js
@@ -5,7 +5,14 @@ const header = ({ title }) => {
     <>
       <Head>
         <title>{title}</title>
-        <meta name="description" content="Todo List App" />
+        <meta
+          name="description"
+          content="Todo List App"
+          property="description"
+        />
+        <meta name="keywords" content="Todo List App" property="keywords" />
+        <meta name="author" content="IvanRomero03" property="author" />
+        <meta name="image" content="./favicon.ico" property="image" />
         <link rel="icon" href="./favicon.ico" />
       </Head>
     </>
